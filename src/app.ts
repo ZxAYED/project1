ca1d442b281864963369912de2a97fb6d4f980ff
Zayed Iqbal <zzayedghost@gmail.com>
2024-11-19 20:43:34 +0600

    Backend setup complete

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+
+import cors from 'cors'
+
+// parser &cors
+app.use(cors())
+app.use(express.json())
+
+type Req = Request
+type Res = Response
+
+app.get('/', (req: Req, res: Res) => {
+  const a = 123
+  res.send(a)
+})
+export default app
