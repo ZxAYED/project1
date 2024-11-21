e5b601bbeb6bd187cd77a0fa2049554cb8573dd8
Zayed Iqbal <zzayedghost@gmail.com>
2024-11-21 17:29:32 +0600

    Added more routes to get student

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { StudentServices } from './student.service';
+import { StudentServices, getAllStudentsFromDb, getSingleStudentsFromDb } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
     try {
@@ -17,8 +17,42 @@ const createStudent = async (req: Request, res: Response) => {
     catch (error) {
         console.log(error);
     }
-
 }
+
+
+
+const getAllStudents = async (req: Request, res: Response) => {
+    try {
+        const result = await StudentServices.getAllStudentsFromDb()
+
+        res.status(200).json({
+            success: true,
+            message: "student are retrieved successfully",
+            data: result
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const getSingleStudent = async (req: Request, res: Response) => {
+    try {
+        const studentId = req.params.studentId
+
+        const result = await StudentServices.getSingleStudentsFromDb(studentId)
+
+        res.status(200).json({
+            success: true,
+            message: "student are retrieved successfully",
+            data: result
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
 export const StundentControllers = {
-    createStudent
+    createStudent, getAllStudents, getSingleStudent
 }
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,4 +5,10 @@ const router = express.Router()
 
 router.post('/create-student', StundentControllers.createStudent)
 
+router.get('/', StundentControllers.getAllStudents)
+router.get('/:studentId', StundentControllers.getSingleStudent)
+
+
+
+
 export const studentRoutes = router
